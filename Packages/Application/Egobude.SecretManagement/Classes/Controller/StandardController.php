e90a25524ec71d5c4934932657351d77c72fddbb
Benedikt Schmitz <benedikt.schmitz@avency.de>
2017-06-20 16:09:42 +0200

    [TASK] Created new package for development

---
+++ Packages/Application/Egobude.SecretManagement/Classes/Controller/StandardController.php
@@ -0,0 +1,24 @@
+<?php
+namespace Egobude\SecretManagement\Controller;
+
+/*
+ * This file is part of the Egobude.SecretManagement package.
+ */
+
+use Neos\Flow\Annotations as Flow;
+use Neos\Flow\Mvc\Controller\ActionController;
+
+class StandardController extends ActionController
+{
+
+    /**
+     * @return void
+     */
+    public function indexAction()
+    {
+        $this->view->assign('foos', array(
+            'bar', 'baz'
+        ));
+    }
+
+}
